9597e9e0046b5cc66d107eeefc8b5787f29213e4
dusanMatijevic <dusanm2401@gmail.com>
2024-03-24 21:58:54 +0100

    Update NsiKlk1SdkCompanyRequestModel.cs

--- NsiProjekat1Sdk.Application/Models/NsiKlk1SdkCompanyRequestModel.cs
+++ NsiProjekat1Sdk.Application/Models/NsiKlk1SdkCompanyRequestModel.cs
@@ -2,12 +2,11 @@
 
 namespace NsiProjekat1Sdk.Application.Models;
 
-public record NsiProjekat1SdkCompanyRequestModel(string Name, string BaseUrl, IDictionary<string, string> Headers)
+public record NsiProjekat1SdkCompanyRequestModel(string Name,string Description,string BaseUrl, IDictionary<string, string> Headers)
 {
     public NsiProjekat1SdkCompanyRequestDto ToDto()
     {
         return new NsiProjekat1SdkCompanyRequestDto(new NsiProjekat1SdkCompanyCreateDto(
-            Name,
-            Name + " Company"));
+            Name,Description));
     }
 }
